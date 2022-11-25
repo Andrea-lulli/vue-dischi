e9d0f00efa94fe7df569402aa7c6558e47924971
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-25 13:50:17 +0100

    card

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="box-conteiner">
+      <BoxCardComp class="" v-for="(elem,index) in libreria" :key="index" :libreria="elem" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BoxCardComp from "./components/BoxCardComp.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    BoxCardComp,
+  },
+  data() {
+    return {
+      libreria: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((resp) => {
+        this.libreria = resp.data.response;
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+#app{
+  display: flex;
+  justify-content: center;
+}
+.box-conteiner{
+  width: 70%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+
+</style>
---
+++ src/components/BoxCardComp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box-card conteiner-fluid">
+    <div class="card p-4" style="">
+      <img :src="libreria.poster" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoxCardComp",
+  props: {
+    libreria: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div{
+  
+  img{
+    width: 100%;
+    aspect-ratio: 1/1;
+    object-position: center;
+  }
+}
+
+.box-card{
+  flex-basis: 19%;
+  margin-bottom: 20px;
+}
+
+</style>
